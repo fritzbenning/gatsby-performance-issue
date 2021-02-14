0b90717978fde551e3bc4d4b60d7dba3aad47427
Fritz Benning <benning@pixelorbit.de>
2021-02-14 16:12:42 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+## Quick Start
+
+🏗 `yarn install` to install dependencies
+
+👷‍♂️ `gatsby develop` to start dev server on <http://localhost:8000>
+
+📦 `gatsby build` to build optimized production-ready static files 
+
+🧪 `gatsby serve` to serve production build for testing
---
+++ gatsby-config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  siteMetadata: {
+    title: `Gatsby Performance Issue`,
+    description: `Test-repo for a weired gatsby performance issue`,
+  },
+  plugins: [
+    `gatsby-plugin-styled-components`,
+    `gatsby-plugin-transition-link`
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
+  ],
+}
